b9e19c5fe1e2063b0f8b8539caa811e91b038603
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2024-01-27 00:42:40 -0500

    firebase

--- server.js
+++ server.js
@@ -1,34 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 const admin = require('firebase-admin');
 
-// Initialize express and setup middleware
-
-
-
-
+// Initialize Firebase Admin SDK
 const serviceAccount = require('./portfolio-13ed0-firebase-adminsdk-vzjz8-9871a56611.json'); // Update the path
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 const db = admin.firestore();
 
+// Initialize express and setup middleware
 const app = express();
-
 app.use(express.json()); // Body parsing middleware
-app.use(cors())
-
-// PostgreSQL connection
-// const pool = new Pool({
-//   user: process.env.DB_USER, //
-//   host: process.env.DB_HOST, // 
-//   database: process.env.DB_DATABASE, // 
-//   password: process.env.DB_PASSWORD, // 
-//   port: process.env.DB_PORT, // 
-// });
+app.use(cors());
 
 // Email sending function
 async function sendEmailNotification(name, userEmail, message) {
@@ -43,7 +29,7 @@ async function sendEmailNotification(name, userEmail, message) {
     // Email to the website owner
     const ownerMailOptions = {
       from: process.env.EMAIL_USERNAME,
-      to: 'robertjguzman15@gmail.com', //
+      to: 'robertjguzman15@gmail.com',
       subject: 'New Portfolio Message',
       text: `You have received a new message from ${name} (${userEmail}): ${message}`,
     };
@@ -51,7 +37,7 @@ async function sendEmailNotification(name, userEmail, message) {
     // Email to the user
     const userMailOptions = {
       from: process.env.EMAIL_USERNAME,
-      to: userEmail, // User's email
+      to: userEmail,
       subject: 'Your Message Has Been Received',
       text: `Hello ${name},\n\nWe have received your message and will get back to you soon. Here's what you sent us:\n\n${message}`,
     };
@@ -66,6 +52,10 @@ async function sendEmailNotification(name, userEmail, message) {
       console.log('Confirmation email sent to user successfully');
     } catch (error) {
       console.error('Error sending email:', error);
+      // Add more error details if available
+      if (error.response) {
+        console.error('SMTP Server responded with:', error.response);
+      }
     }
 }
 
