bf0c9f0cbf37a20b9b515be26c13a02b9bfe6375
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2024-07-02 05:01:59 -0400

    build added to the server

--- api/server.js
+++ api/server.js
@@ -124,7 +124,7 @@ app.get("/api/messages", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
